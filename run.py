27afb6d591478836393c799931c1c19a40e75b06
brendinhafarias <brenda.fariascs@gmail.com>
2026-03-05 18:41:47 -0300

    Fix run.py port and gunicorn config

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import os
 from app import create_app
 
-app = create_app(os.environ.get('FLASK_ENV', 'development'))
+app = create_app(os.environ.get('FLASK_ENV', 'production'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
